913be40e027353393f4a5d77e83a72b0397c95f3
adansito10 <adandejesus200420@gmail.com>
2024-07-24 17:29:04 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('registrationForm');
+
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+    }, false);
+});
